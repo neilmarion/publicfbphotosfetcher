2d61194a13ca4a0662ec957e0e310edeebb03e4e
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2013-12-25 13:06:00 +0800

    Fixing up some filename issues

--- downloader.rb
+++ downloader.rb
@@ -1,15 +1,17 @@
 require "open-uri"
 
+counter = 0
 File.open("photos", "r").each_line do |line|
   l = line.split('/')[4]
   #f = File.open("cfn/#{l}", "w")
 
   open(line) {|f|
-     File.open("cfn/#{l}","w+") do |file|
+     File.open("cfn/#{counter}.jpg","wb") do |file|
        puts "downloading #{line}"
        file.puts f.read
        file.close
      end
+     counter=counter+1
   }
 end
 
